048d557bff87e9e69a3d792f25531bbb0cafc36a
Felipe-CE <felipe_rsr@live.com>
2021-03-17 19:00:33 -0300

    Update

--- animais fantasticos/prototype.js
+++ animais fantasticos/prototype.js
@@ -1,13 +1,89 @@
-function Pessoa(nome, idade) {
-  this.nome = 'nome';
-  this.idade = 0;
-}
+// function Pessoa(nome, sobrenome, idade) {
+//   this.nome = nome;
+//   this.sobrenome = sobrenome;
+//   this.idade= idade;
+// }
 
-Pessoa.prototype.andar = function () {
-  return `pessoa andou`
-}
+// Pessoa.prototype.nomeCompleto = function () {
+//   return `${this.nome} ${this.sobrenome}`;
+// }
 
-const felipe = new Pessoa('felipe', 29);
+// const felipe = new Pessoa('felipe', 'xavier', 29);
 
-console.log(Pessoa.prototype)
-console.log(felipe.prototype)
+// const li = document.querySelector('.menu li')
+
+// if (typeof document.webkitHidden !== 'undefined') {
+//   console.log('existe')
+// }
+
+// Utilizando o foreach na array abaixo,
+// some os valores de Taxa e os valores de Recebimento
+
+const transacoes = [
+  {
+    descricao: 'Taxa do Pão',
+    valor: 'R$ 39',
+  },
+  {
+    descricao: 'Taxa do Mercado',
+    valor: 'R$ 129',
+  },
+  {
+    descricao: 'Recebimento de Cliente',
+    valor: 'R$ 99',
+  },
+  {
+    descricao: 'Taxa do Banco',
+    valor: 'R$ 129',
+  },
+  {
+    descricao: 'Recebimento de Cliente',
+    valor: 'R$ 49',
+  },
+];
+
+let taxaTotal = 0
+let recebimentoTotal = 0
+transacoes.forEach((item) =>{
+  const numeroLimpo = Number(item.valor.replace('R$ ', ''))
+
+
+  if (item.descricao.slice(0, 4) === 'Taxa') {
+    taxaTotal += numeroLimpo;
+  } else if (item.descricao.slice(0, 4) === 'Rece'){
+    recebimentoTotal += numeroLimpo
+  }
+})
+console.log(taxaTotal)
+console.log(recebimentoTotal)
+
+// Retorne uma array com a lista abaixo
+const transportes = 'Carro Avião Trem Ônibus Bicicleta';
+const arrayTransporte = transportes.split(' ')
+console.log(arrayTransporte)
+
+// Substitua todos os span's por a's
+let html = `<ul>
+                <li><span>Sobre</span></li>
+                <li><span>Produtos</span></li>
+                <li><span>Contato</span></li>
+              </ul>`;
+
+html = html.split('span').join('a')
+console.log(html)
+
+// Retorne o último caracter da frase
+const frase = 'Melhor do ano!';
+console.log(frase.slice(-1))
+
+// Retorne o total de taxas
+let totalTaxa = 0;
+const transacoes1 = ['Taxa do Banco', '   TAXA DO PÃO', '  taxa do mercado', 'depósito Bancário', 'TARIFA especial'];
+transacoes1.forEach((item) =>{
+  item = item.toLocaleLowerCase().trim().slice(0, 4)
+
+  if (item === 'taxa') {
+    totalTaxa++
+  }
+})
+console.log(totalTaxa)
