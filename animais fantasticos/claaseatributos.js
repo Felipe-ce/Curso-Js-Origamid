fd6721002b0a0da7d76953cca6aca17fae7b2d42
Felipe-ce <felipe_rsr@live.com>
2021-10-14 22:00:08 -0300

    update

--- animais fantasticos/claaseatributos.js
+++ animais fantasticos/claaseatributos.js
@@ -1,19 +1,24 @@
-const menu = document.querySelectorAll('.menu a')
-menu.forEach((item) =>{
-  item.classList.add('ativo')
-})
 
-menu.forEach((item) =>{
+
+// Adicione a classe ativo a todos os itens do menu
+const menu = document.querySelectorAll('.menu a')
+menu.forEach((item) => {
+  item.classList.add('ativo')
+});
+
+// Remove a classe ativo de todos os itens do menu e mantenha apenas no primeiro
+menu.forEach((item) => {
   item.classList.remove('ativo')
-})
+});
 
 menu[0].classList.add('ativo')
+// Verifique se as imagens possuem o atributo alt
+const img = document.querySelectorAll('img')
+img.forEach((item) =>(
+  console.log(item.hasAttribute('alt'))
+))
 
-const allImg = document.querySelectorAll('img');
-allImg.forEach((img, index) =>{
-  const possuiAlt = img.hasAttribute('alt')
-  console.log(img, possuiAlt)
-})
-
+// Modifique o href do link externo no menu
 const linkExterno = document.querySelector('a[href^="http"]')
+console.log(linkExterno)
 linkExterno.setAttribute('href', 'https://felipeads.com/')
